5085d0d7fc8768442e838800d8bb6aa6ecbf5fa3
trieund2 <trieund2@vng.com.vn>
2020-02-03 09:45:56 +0700

    Add Atom Class

---
+++ iOS_Animation/iOS_Animation/Atom.h
@@ -0,0 +1,17 @@
+//
+//  Atom.h
+//  iOS_Animation
+//
+//  Created by LAP12852 on 2/3/20.
+//  Copyright © 2020 LAP12852. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Atom : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS_Animation/iOS_Animation/Atom.m
@@ -0,0 +1,13 @@
+//
+//  Atom.m
+//  iOS_Animation
+//
+//  Created by LAP12852 on 2/3/20.
+//  Copyright © 2020 LAP12852. All rights reserved.
+//
+
+#import "Atom.h"
+
+@implementation Atom
+
+@end
