3b817ef83893f645cc5bd5db27287bd7fb507c22
trieund2 <trieund2@vng.com.vn>
2020-02-03 09:44:45 +0700

    Add test VC

---
+++ iOS_Animation/iOS_Animation/TestViewController.h
@@ -0,0 +1,17 @@
+//
+//  TestViewController.h
+//  iOS_Animation
+//
+//  Created by LAP12852 on 2/3/20.
+//  Copyright © 2020 LAP12852. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS_Animation/iOS_Animation/TestViewController.m
@@ -0,0 +1,32 @@
+//
+//  TestViewController.m
+//  iOS_Animation
+//
+//  Created by LAP12852 on 2/3/20.
+//  Copyright © 2020 LAP12852. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
